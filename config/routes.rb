6ba3d1af123e2e66db046781437c84cb06485f5d
connectica <heba@connectica.no>
2016-08-03 19:44:49 +0700

    creating projects pages; adding their routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   root to: 'home#index'
   get '/projects', to: 'projects#index', as: 'projects'
+  get '/projects/:id', to: 'projects#show', as: 'project'
   get '/about', to: 'about#index', as: 'about'
 
 end
