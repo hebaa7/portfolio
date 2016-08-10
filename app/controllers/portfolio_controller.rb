d5477a8d5b9d5118941dd41355e960e64feb94a0
connectica <heba@connectica.no>
2016-08-10 19:11:35 +0700

    destroyed projects controller; added portfolio controller

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -1,4 +1,4 @@
-class ProjectsController < ApplicationController
+class PortfolioController < ApplicationController
 
   def index
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ Rails.application.routes.draw do
   root to: 'home#index'
   get '/about', to: 'about#index', as: 'about'
 
-  get '/projects', to: 'projects#index', as: 'projects'
-  get '/projects/heba'
-  get '/projects/uteeni'
-  get '/projects/harvester'
-  get '/projects/connectica'
-  get '/projects/hello'
-  get '/projects/raja'
+  get '/portfolio', to: 'portfolio#index', as: 'portfolio'
+  get '/portfolio/heba'
+  get '/portfolio/uteeni'
+  get '/portfolio/harvester'
+  get '/portfolio/connectica'
+  get '/portfolio/hello'
+  get '/portfolio/raja'
 
 
 end
