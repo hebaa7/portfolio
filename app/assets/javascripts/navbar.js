3a01614f73617b2a6826eefaab4fa2190bd7044d
connectica <heba@connectica.no>
2016-08-13 13:24:04 +0700

    styled navbar in home and portfolio

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -6,12 +6,12 @@ $(document).ready(function(){
       scroll_start = $(this).scrollTop();
       if(scroll_start > offset.top) {
           $('#main-nav').css({'background-color': '#fff', '-webkit-box-shadow': '0px 5px 20px -5px rgba(79,79,79,1)', '-moz-box-shadow': '0px 5px 20px -5px rgba(79,79,79,1)', 'box-shadow': '0px 5px 20px -5px rgba(79,79,79,1)'});
-          $('.nav-item').css({'color': '#000', 'font-weight': '500'});
-          $('.fa-stack').css('color', '#000');
+          $('#main-nav .nav-item').css({'color': '#2F4148', 'font-weight': '500'});
+          $('#main-nav .nav-icon').css('color', '#2F4148');
        } else {
           $('#main-nav').css({'background-color': 'transparent', '-webkit-box-shadow': 'none', '-moz-box-shadow': 'none', 'box-shadow': 'none'});
-          $('.nav-item').css({'color': '#fff', 'font-weight': '300'});
-          $('.fa-stack').css('color', '#fff');
+          $('#main-nav .nav-item').css({'color': '#fff', 'font-weight': '300'});
+          $('#main-nav .nav-icon').css('color', '#fff');
        }
    });
 });
