8749d35e27266347bb9884a888092914186e623a
connectica <heba@connectica.no>
2016-08-14 19:13:07 +0700

    testing navbar

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -25,7 +25,7 @@ $(document).ready(function(){
           });
           $('#main-nav .nav-item').css({
             'color': '#fff',
-            'font-weight': '400'
+            'font-weight': '300'
           });
           $('#main-nav .nav-icon').css('color', '#fff');
        }
